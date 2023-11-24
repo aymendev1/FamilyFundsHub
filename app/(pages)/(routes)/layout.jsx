122d5dbc4d05b30b8a1c33c0e53ef3eb8748b642
Ayman Azougar <topayman5@gmail.com>
2023-11-24 16:56:23 +0100

    Add Childrens to layout page

--- app/(pages)/(routes)/layout.jsx
+++ app/(pages)/(routes)/layout.jsx
@@ -4,6 +4,7 @@ function layout({ children }) {
   return (
     <div className="w-full h-screen flex bg-blue-50 ">
       <Sidebar />
+      <section className=" w-full min-h-full p-6">{children}</section>
     </div>
   );
 }
