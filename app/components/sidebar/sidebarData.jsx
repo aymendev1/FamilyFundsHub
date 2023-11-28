4b9b7941d0b43b1d5636d7d753e6c582f4605163
Ayman Azougar <topayman5@gmail.com>
2023-11-28 13:18:22 +0000

    Edit Sidebar menu data

--- app/components/sidebar/sidebarData.jsx
+++ app/components/sidebar/sidebarData.jsx
@@ -1,19 +1,15 @@
 import React from "react";
 import Link from "next/link";
-import {
-  HomeIcon,
-  CreditCardIcon,
-  Cog6ToothIcon,
-  ArrowRightOnRectangleIcon,
-} from "@heroicons/react/24/solid";
+import { FaMoneyBillTransfer,FaUsers,FaArrowRightFromBracket } from "react-icons/fa6";
+import { MdSavings,MdAccountBalanceWallet,MdSettings,MdHome } from "react-icons/md";
 function sidebarData(props) {
   const Data = [
-    { icon: HomeIcon, text: "Dashboard", link: "/dashboard" },
-    { icon: CreditCardIcon, text: "To decide", link: "#" },
-    { icon: CreditCardIcon, text: "To decide", link: "#" },
-    { icon: CreditCardIcon, text: "To decide", link: "#" },
-    { icon: CreditCardIcon, text: "To decide", link: "#" },
-    { icon: Cog6ToothIcon, text: "Settings", link: "/settings" },
+    { icon: MdHome, text: "Dashboard", link: "/dashboard" },
+    { icon: MdAccountBalanceWallet, text: "Balance", link: "/balance" },
+    { icon: MdSavings, text: "Savings", link: "/savings" },
+    { icon: FaMoneyBillTransfer, text: "Expenses", link: "/expenses" },
+    { icon: FaUsers, text: "Family", link: "#" },
+    { icon: MdSettings, text: "Settings", link: "/settings" },
   ];
   return (
     <>
@@ -34,7 +30,7 @@ function sidebarData(props) {
       </div>
       {/* Logout button */}
       <div className=" flex items-center mt-2 p-4 gap-2 h-[3.5rem] rounded-lg cursor-pointer text-blue-950 hover:bg-blue-400 hover:text-white transition-all duration-300">
-        <ArrowRightOnRectangleIcon className="min-w-[25px] h-[25px] " />
+        <FaArrowRightFromBracket className="min-w-[25px] h-[25px] " />
         <Link href="/logout" className={` font-medium `}>
           Log out
         </Link>
