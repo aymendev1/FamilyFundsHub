55e622e1dc67a2f55661aba7cd0c664acfc2a091
Ayman Azougar <topayman5@gmail.com>
2023-11-28 22:25:04 +0100

    Add " LatestTransactionsTable" component

--- app/(pages)/(routes)/dashboard/page.jsx
+++ app/(pages)/(routes)/dashboard/page.jsx
@@ -5,6 +5,7 @@ import CreditCard from "@/app/components/cards/CreditCard";
 import StatsCardEarning from "@/app/components/cards/StatsCardEarning";
 import OverviewChartDashboard from "@/app/components/charts/overviewChartDashboard";
 import FamilyChartCard from "@/app/components/cards/FamilyChartCard";
+import LatestTransactionsTable from "@/app/components/tables/LatestTransactionsTable";
 function page() {
   return (
     <>
@@ -17,7 +18,6 @@ function page() {
           {/* Cards Shows Balance */}
           <div className="flex flex-row w-full gap-2 ">
             <div className="flex-1">
-              {" "}
               <CreditCard />
             </div>
             <div className="flex-1 flex gap-2 justify-between max-xl:h-[200px] max-lg:w-full flex-col">
@@ -39,6 +39,15 @@ function page() {
         </div>
         {/* FamilyChart */}
         <FamilyChartCard />
+      </div>
+      <div className="flex bg-white rounded-lg mt-8 flex-col p-4 gap-2  max-lg:flex-col">
+        <span className="text-xl  font-black  text-blue-950">
+          Latest transactions
+        </span>
+        <span className="text-md text-slate-700">
+          Your latest transactions{" "}
+        </span>
+        <LatestTransactionsTable />
       </div>
     </>
   );
