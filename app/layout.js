9221a28a0f8b022c38c5799555bb84b20fcd26a7
Ayman Azougar <topayman5@gmail.com>
2023-11-28 22:25:52 +0100

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className="h-full">
-      <body className="h-full">{children}</body>
+    <html lang="en" className="min-h-full">
+      <body className="min-h-full">{children}</body>
     </html>
   );
 }
