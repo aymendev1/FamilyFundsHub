6bf36574ca040a73dae7b2d046c781b3730508c1
Ayman Azougar <topayman5@gmail.com>
2023-12-01 13:47:38 +0100

    Create ActionMenu.jsx
    - initialise the component

---
+++ app/components/Menus/ActionMenu.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ActionMenu() {
+  return (
+    <div className="flex flex-col gap-4 w-[30%] h-[631px] max-lg:w-full  bg-white rounded-lg p-4">
+      ActionMenu
+    </div>
+  );
+}
+
+export default ActionMenu;
