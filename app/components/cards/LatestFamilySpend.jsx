62454be6cc0b7f336b5e9f5b4d58000b9398346a
Ayman Azougar <topayman5@gmail.com>
2023-12-06 23:04:21 +0100

    Create LatestFamilySpend.jsx
    Init component

---
+++ app/components/cards/LatestFamilySpend.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LatestFamilySpend() {
+  return (
+    <div className="flex flex-col gap-4 w-[30%] h-[631px] max-lg:w-full  bg-white rounded-lg p-4">
+      LatestFamilySpend
+    </div>
+  );
+}
+
+export default LatestFamilySpend;
