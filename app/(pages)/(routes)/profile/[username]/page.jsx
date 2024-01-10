29d3fd79d3b184ab44ea32f97efd13f3b4bb89b7
Ayman Azougar <topayman5@gmail.com>
2024-01-10 13:49:41 +0100

    Update ProfilePage
    - Remove LastSpentCard

--- app/(pages)/(routes)/profile/[username]/page.jsx
+++ app/(pages)/(routes)/profile/[username]/page.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import ProfileComponent from "@/app/components/profile/ProfileComponent";
-import LatestFamilySpend from "@/app/components/cards/LatestFamilySpend";
-function page() {
+function page({ params }) {
+  const { username } = params;
   return (
     <>
       <div className="pb-5">
         <span className="text-3xl  font-black  text-blue-950 ">Profile</span>
       </div>
       <div className="flex flex-row max-md:flex-col gap-10 max-lg:gap-6 max-sm:gap-4">
-        <ProfileComponent />
-        <LatestFamilySpend />
+        <ProfileComponent username={username} />
       </div>
     </>
   );
